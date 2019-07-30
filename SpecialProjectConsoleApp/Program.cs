78d4a695421d2f12ff656158584bfdbd3c5dbf48
Vadim Gorfung <gorfungs@yaclass.eu>
2019-07-30 08:48:56 +0300

    Made changes

--- SpecialProjectConsoleApp/Program.cs
+++ SpecialProjectConsoleApp/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string textToShow = "are you sure ?";
+
             Console.WriteLine("Hello world");
             Console.ReadLine();
         }
